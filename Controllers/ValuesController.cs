c7b0019cbce4847ac57d042b3cf731a47ac6d2bd
Dylan C <hchen004@citymail.cuny.edu>
2017-07-31 21:33:37 -0400

    Change Documentation home page; change HTTP request structure based on the GlassdoorAPI project

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -9,15 +9,16 @@ namespace Test.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // GET api/values/google
+        // GET api/values/SearchQuery = 
         public DataModel Get(string jobfield)
         {
             Glassdoor glassdoor = new Glassdoor(jobfield);
+            glassdoor.SetCredentials("[Partner ID]", "[API Key]");
             string content = glassdoor.Search();
             return JsonConvert.DeserializeObject<DataModel>(content);
         }
@@ -25,18 +26,18 @@ namespace Test.Controllers
         
 
         // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
